ae9d149c03c02aca71254455b850f0b730787f4f
Egidijus Gecius <e.gecius@gmail.com>
2020-03-28 14:15:33 +0000

    simplify MainActivity

--- app/src/main/java/com/egecius/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/egecius/viewmodeldemo/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.egecius.viewmodeldemo
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -9,19 +8,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val model: MyViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+        updateOnDataChanges()
+    }
 
-        val model: MyViewModel by viewModels()
-
-
-        increment_button.setOnClickListener {
-            model.onButtonClicked()
-        }
-
+    private fun updateOnDataChanges() {
         model.counter.observe(this, Observer {
             counter_text_view.text = it.toString()
         })
+    }
+
+    private fun setListeners() {
+        increment_button.setOnClickListener {
+            model.onButtonClicked()
+        }
     }
 }
