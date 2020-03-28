7f22779b2065f4657408d17d541b42dc81a1a14d
Egidijus Gecius <e.gecius@gmail.com>
2020-03-28 14:04:18 +0000

    inject SavedStateHandle into MyViewModel

--- app/src/main/java/com/egecius/viewmodeldemo/MyViewModel.kt
+++ app/src/main/java/com/egecius/viewmodeldemo/MyViewModel.kt
@@ -1,10 +1,18 @@
 package com.egecius.viewmodeldemo
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MyViewModel : ViewModel() {
+// saved state goes here into SavedStateHandle
+class MyViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    
+    init {
+        Log.v("Eg:MyViewModel:11", "() savedStateHandle: $savedStateHandle")
+    }
+
     private val users: MutableLiveData<List<User>> = MutableLiveData(getDefaultUsers())
 
     private fun getDefaultUsers(): List<User> {
